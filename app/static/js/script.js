8eb8d1244d200582d8d917ef397bf88ede2f5a8f
Elefantat <marballeee@gmail.com>
2024-05-13 23:06:05 +0800

    run prettier format

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,35 +1,34 @@
 function socialSignUp(platform) {
-    let redirectUrl;
-  
-    switch (platform) {
-      case 'twitter':
-        redirectUrl = 'https://twitter.com/signup';
-        break;
-      case 'google':
-        redirectUrl = 'https://accounts.google.com/signup';
-        break;
-      case 'facebook':
-        redirectUrl = 'https://www.facebook.com/r.php';
-        break;
-      default:
-        console.log('Invalid social platform');
-        return;
-    }
-  
-    window.location.href = redirectUrl;
+  let redirectUrl;
+
+  switch (platform) {
+    case "twitter":
+      redirectUrl = "https://twitter.com/signup";
+      break;
+    case "google":
+      redirectUrl = "https://accounts.google.com/signup";
+      break;
+    case "facebook":
+      redirectUrl = "https://www.facebook.com/r.php";
+      break;
+    default:
+      console.log("Invalid social platform");
+      return;
   }
 
+  window.location.href = redirectUrl;
+}
 
 /* Checkbox for Create page */
-const checkbox = document.getElementById('checkbox');
-const submitBtn = document.querySelector('.PostSubmitbtn');
+const checkbox = document.getElementById("checkbox");
+const submitBtn = document.querySelector(".PostSubmitbtn");
 
-submitBtn.addEventListener('click', () => {
+submitBtn.addEventListener("click", () => {
   if (checkbox.checked) {
-    console.log('Checkbox is checked');
+    console.log("Checkbox is checked");
     // Add additional functionality here
   } else {
-    console.log('Checkbox is not checked');
+    console.log("Checkbox is not checked");
     // Add additional functionality here
   }
 });
